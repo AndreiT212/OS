1e8f735caa4fdf967f0c3632083b08826773a3d4
andrew <andreitatov212@gmail.com>
2023-04-20 16:38:46 +0300

    Added libraries

--- main.c
+++ main.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>  
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+ 
+
 
 int main(int argc, char* argv[])
 {
